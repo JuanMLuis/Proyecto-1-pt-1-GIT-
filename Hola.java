e37fdf33ef6a03a2869941117fedcb143386c91e
JuanMLuis <juanmaluis98@gmail.com>
2020-09-17 17:49:15 -0300

    agrego 2 archivos

---
+++ Hola.java
@@ -0,0 +1,8 @@
+
+public class Hola {
+	
+	
+	public void hola() {
+		System.out.println("Hola Mundo");
+	}
+}
---
+++ hola2.java
@@ -0,0 +1,12 @@
+
+public class hola2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Hola h= new Hola();
+		h.hola();
+
+	}
+
+}
